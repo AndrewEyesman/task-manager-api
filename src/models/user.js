b902da3e657a30484937d821ca2789a32899207c
AndrewEyesman <andreweyesman@gmail.com>
2019-12-30 17:03:47 -0500

    idk what im doing

--- src/models/user.js
+++ src/models/user.js
@@ -15,7 +15,7 @@ const userSchema =  new mongoose.Schema({
         default: 0,
         validate(value) {
             if (value < 0) {
-                throw new Error('Age must be a positive number');
+                throw new Error('Age must be a positive number')
             } 
         }
     },
